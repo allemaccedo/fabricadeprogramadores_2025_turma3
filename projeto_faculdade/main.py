5ef5e8e76f9baff40542a571983a70b6dc06bd08
Alessandro Macedo <macedoalessandro016@gmail.com>
2025-09-30 15:34:16 -0300

    Update main.py

--- projeto_faculdade/main.py
+++ projeto_faculdade/main.py
@@ -1,52 +1,69 @@
-from tabelas import SessionLocal, Usuario, Nota
+from tabelas import SessionLocal, Usuario, Nota, joinedload
 
 db = SessionLocal()
 
-def criar_novo_usuario_e_nota():
-    """Exemplo de como CRIAR dados."""
-    novo_usuario = Usuario(
-        nome = "Alessandro Macedo",
-        email= "macedoalessandro0a16@gmail.com",
-        senha_hash= "hash_super_seguro"  
-    )
+def criar_novo_usuario_e_nota(novo_usuario: Usuario, nova_nota: Nota):
 
     db.add(novo_usuario)
     db.commit()
     print(f"Usuario '{novo_usuario.nome}' criado com ID: {novo_usuario.id}")
 
-    nova_nota = Nota(
-        conteudo="Minha Primeira Nota com SQLAlchemy",
-        autor=novo_usuario
+    note = Nota(
+        id_usuario=novo_usuario.id,
+        titulo=nova_nota.titulo,
+        conteudo=nova_nota.conteudo
     )
-    db.add(nova_nota)
-    db.commit
-    print(f"Nota '{nova_nota.conteudo}' criada para {novo_usuario.nome}")
+    db.add(note)
+    db.commit()
 
-def ler_dados(nome):
-    """Exemplo de como  LER dados."""
 
-    user= db.query(Usuario).filter(Usuario.nome == nome ).filter()
+def atualizar_nota(id_nota: int, titulo:str, conteudo: str):
 
-    if user:
-        print(f"Encontrei o(a): {user.nome} (Email: {user.email})")
-        print("Notas do user:")
-        for nota in user.notas:
-            print(f"  - conteudo: {nota.conteudo} (ID {nota.id})")
-
-    else:
-        print("Usuario(a) nao encontrado.")
-
-def autualizar_nota(id_nota):
-    """Exemplo de como ATUALIZAR dados."""
-    
     nota_para_editar = db.query(Nota).filter(Nota.id == id_nota).first()
 
     if nota_para_editar:
-        print(f"conteudo original: '{nota_para_editar.conteudo}'")
-        nota_para_editar= "Lista de anotações ATUALIZADA!"
+       
+        nota_para_editar.titulo=titulo
+        nota_para_editar.conteudo=conteudo
+
         db.commit()
-        print(f"conteudo novo: '{nota_para_editar.conteudo}'")
     else:
         print("Nota com ID % não encontrada." % id_nota)
 
-criar_novo_usuario_e_nota()
+def ler_dados():
+
+    users = db.query(Usuario).options(joinedload(Usuario.notas)).all()
+
+    resultado = []
+    for u in users:
+        notas = []
+        for n in u.notas:
+            notas.append({
+                "id": n.id,
+                "titulo": n.titulo,
+                "conteudo": n.conteudo,
+                "criado_em": n.criado_em
+            })
+
+
+            resultado.append({
+            "id": u.id,
+            "usuario": u.nome,
+            "email": u.email,
+            "criado_em": u.criado_em,
+            "notas": notas
+        })
+    return resultado
+
+def deletar_usuario(id_usuario: int):
+
+    usuario_deletado = db.query(Usuario).filter(Usuario.id == id_usuario).first()
+
+    if usuario_deletado:
+
+        db.delete(usuario_deletado)
+        db.commit()
+
+        print(f"Usuario: '{usuario_deletado.nome}' removido com sucesso!")       
+    else:
+        print("Nota com ID % não encontrada." % id_usuario)
